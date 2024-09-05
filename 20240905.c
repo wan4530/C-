2e8324c737090ffde351c00e7f677140cf48af43
wan4530 <130379941+wan4530@users.noreply.github.com>
2024-09-05 11:59:48 +0900

    20240905.c

--- 20240905.c
+++ 20240905.c
@@ -3,24 +3,22 @@
 #include <stdio.h>
 
 int main(){
-    int n, x;
+    int n,id;
     int a[100] = { 0, };
     int b[100] = { 0, };
+    int c[100] = { 0, };
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
     for (int i = 0; i < n; i++) {
         scanf("%d", &b[i]);
+        c[i] = a[i] - b[i];
     }
-    scanf("%d", &x);
+    scanf("%d", &id);
+    printf("%d\n", c[id - 1]);
     for (int i = 0; i < n; i++) {
-        a[i] -= b[i];
+        printf("%d ", c[i]);
     }
-    printf("%d\n", a[x - 1]);
-    for (int i = 0; i < n; i++) {
-        printf("%d ", a[i]);
-    }
-
     return 0;
 }
