d737d92de2287300e124b17ce2272599e838978d
wan4530 <130379941+wan4530@users.noreply.github.com>
2024-09-20 22:57:17 +0900

    Add files via upload

---
+++ 3_3.c
@@ -0,0 +1,24 @@
+#pragma warning(disable:4996)
+#pragma warning(disable:6031)
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 100
+
+void inputRandomArray(int* array, int size) {
+	for (int i = 0; i < size; i++) {
+		*(array + i) = rand() % 100;
+	}
+}
+void printArray(int* array, int size) {
+	for (int i = 0; i < size; i++) {
+		printf("%3d\t", *(array+i));
+		if ((i + 1) % 10 == 0) printf("\n");
+	}
+}
+int main(){
+	int number[SIZE];
+	srand(time(NULL));
+	inputRandomArray(number, SIZE);
+	printArray(number, SIZE);
+}
