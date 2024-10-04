fc5ed6631ef22502df582edbd8bc82776c91faaf
wan4530 <130379941+wan4530@users.noreply.github.com>
2024-10-04 22:23:30 +0900

    Add files via upload

---
+++ 5_3.c
@@ -0,0 +1,25 @@
+#pragma warning(disable:4996)
+#pragma warning(disable:6031)
+#include <stdio.h>
+int main(void) {
+    char input[200] = "";
+    int count[26] = { 0, };
+    int ch;
+    int i = 0;
+
+    while ((ch = getchar()) != EOF) {
+        input[i++] = ch;
+    }
+    i = 0;
+    while (input[i] != '\0') {
+        if (input[i] >= 'a' && input[i] <= 'z') {
+            count[input[i]-'a']++;
+        }
+        i++;
+    }
+    printf("입력문자열 : %s\n", input);
+    for (int i = 0; i < 26; i++) {
+        printf("%c 출현횟수 : %d\n", 'a' + i, count[i]);
+    }
+    return 0;
+}
