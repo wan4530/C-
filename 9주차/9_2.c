1034d1a23f69cda341fc278ef336381c1baf8e44
wan4530 <130379941+wan4530@users.noreply.github.com>
2024-11-07 21:11:48 +0900

    Add files via upload

--- 9주차/9_2.c
+++ 9주차/9_2.c
@@ -1,6 +1,7 @@
 #pragma warning(disable:4996)
 #pragma warning(disable:6031)
 #include <stdio.h>
+
 int getline(char* line) {
 	int ch;
 	int i = 0;
@@ -10,17 +11,25 @@ int getline(char* line) {
 }
 int main(void) {
 	char input[100];
-	FILE* fp;
-	if (((fp = fopen("output.txt", "r")) == NULL)) {
+	FILE* fp_src;
+	FILE* fp_dest;
+
+	if (((fp_src = fopen("output.txt", "r")) == NULL)) {
 		printf("error...");
 		return 0;
 	}
-	while (!feof(fp)) {
-		fgets(input, 100, fp);
-		printf("%s", input);
+	if (((fp_dest = fopen("output2.txt", "w")) == NULL)) {
+		printf("error...");
+		return 0;
+	}
+	while (!feof(fp_src)) {
+		fgets(input, 100, fp_src);
+		printf("Ãâ ·Â Áß\n");
+		fputs(input, fp_dest);
 	}
 
-	fclose(fp);
+	fclose(fp_src);
+	fclose(fp_dest);
 
 	return 0;
 }
